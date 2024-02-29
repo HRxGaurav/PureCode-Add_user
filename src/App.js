79d9053079c310caa429bd2920085ab7cf92f279
hrxgaurav <magicgalaxy0@gmail.com>
2024-02-29 15:11:20 +0530

    1.0

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import DashboardLayout from './components/sidebar/DashboardLayout';
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import useSettings from './hooks/useSettings';
 import {createCustomTheme} from './theme'
-import AddNewUser from './pages/add-new-user';
 import RTL from './components/input-fields/RTL';
 import StyledEngineProvider from "@mui/material/StyledEngineProvider";
 import "./i18n";
